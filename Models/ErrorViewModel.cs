fa570be40d42e0c109dcdde9c1bccfebfcc403ed
Clément <clemm.favre@gmail.com>
2026-01-27 21:40:09 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelpDeskWebapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
